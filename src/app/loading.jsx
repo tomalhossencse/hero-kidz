bd375cd99ef82275ceeba7ff7decb800a2713ae8
Tomal Hossen <tomalhossen78@gmail.com>
2026-04-17 10:48:59 +0600

    feat : seo friendly metadat added

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const loading = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center text-5xl font-bold">
-      <p>HERO</p>
+    <div className="min-h-screen flex items-center justify-center text-5xl font-bold animate-pulse">
+      <p className="animate-ping text-2xl">HER</p>
       <span className="animate-spin">🧿</span>
       <p className="text-primary">KIDZ...</p>
     </div>
